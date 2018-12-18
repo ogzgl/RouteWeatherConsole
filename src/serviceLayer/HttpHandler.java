83acc149e90eb3aded331118bfdf11efd1b6c031
ogzgl <oguz_gul@hotmail.com.tr>
2018-12-18 20:36:23 +0300

    external libraries, http handler improvement

--- src/serviceLayer/HttpHandler.java
+++ src/serviceLayer/HttpHandler.java
@@ -1,6 +1,9 @@
 package serviceLayer;
 
-import org.json.JSONObject;
+import Exceptions.Exceptions;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,19 +11,32 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpHandler {
-    public JSONObject request(String address) throws IOException {
+    public JsonNode request(String address) throws IOException,
+            Exceptions.BadRequestException,
+            Exceptions.NotFoundLocation,
+            Exceptions.ConnectionError {
         URL url = new URL(address);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         connection.addRequestProperty("Content-Type","application/json");
-        int responseCode = connection.getResponseCode();
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         StringBuilder response = new StringBuilder();
+        int responseCode = connection.getResponseCode();
         String inputLine;
         while((inputLine = in.readLine())!=null){
             response.append(inputLine);
         }
         in.close();
-        return new JSONObject(response.toString());
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonResponse = mapper.readTree(response.toString());
+        if (jsonResponse.get("status").asText().equals("NOT_FOUND")) {
+            throw new Exceptions.NotFoundLocation("Given location is not valid.", Exceptions.NotFoundLocation);
+        } else if (responseCode == 400) {
+            throw new Exceptions.BadRequestException("Given location was not valid for Dark Sky.", Exceptions.BadRequestException);
+        } else if (responseCode == 200) {
+            return jsonResponse;
+        } else {
+            throw new Exceptions.ConnectionError("Connection failed.", Exceptions.ConnectionError);
+        }
     }
 }
